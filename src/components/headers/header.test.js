32463ef35b7265ee51f6e84b1a4c0b3396a5045f
Kedar Lachke <kedarlachke1@gmail.com>
2020-09-22 11:55:10 +0800

    header component test case

---
+++ src/components/headers/header.test.js
@@ -0,0 +1,31 @@
+  
+import React from 'react';
+import { shallow } from 'enzyme';
+//import { findByTestAtrr } from './../../../Utils';
+import Header from './index';
+import {findByTestAtrr} from '../../../utils'
+
+const setUp=(props)=>{
+    const component = shallow(<Header {...props}/>);
+    return component;
+}
+
+
+describe("Header Component",()=>{
+    let component;
+    beforeEach(()=>{component=setUp()})
+    it('should render without errors',()=>{
+       const wrapper=findByTestAtrr(component,'headerComponent');
+       expect(wrapper.length).toBe(1);
+
+    });
+
+    it('should render a logo',()=>{
+       const logo=findByTestAtrr(component,'logoIMG');
+       expect(logo.length).toBe(1);
+
+    });
+
+})
+
+
--- src/components/headers/index.js
+++ src/components/headers/index.js
@@ -3,10 +3,10 @@ import './styles.scss'
 import Logo from '../../assets/logo.png'
 function Header() {
     return (
-        <header>
+        <header data-test="headerComponent">
         <div className="wrap">
         <div className="logo">
-            <img src={Logo} alt="logo"></img>
+            <img src={Logo} alt="logo" data-test="logoIMG"></img>
         </div>
         </div>   
         </header>

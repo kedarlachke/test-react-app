fc7976083d045dbd7580eea50036deda12a0d54e
Kedar Lachke <kedarlachke1@gmail.com>
2020-09-23 14:41:00 +0800

    Simulate Event in Test cases

--- src/components/button/button.test.js
+++ src/components/button/button.test.js
@@ -17,16 +17,26 @@ describe("SharedButton Component",()=>{
 
     describe("Render",()=>{
         let wrapper;
+        let mockfun;
         beforeEach(()=>{
+            mockfun=jest.fn();
             const props ={
                 buttonText:'Example button text',
-                emitEvent:()=>{    }                
+                emitEvent:mockfun                
             }
             wrapper =shallow(<SharedButton {...props}/>)
         })
         it("should render an button",()=>{
             const button = findByTestAtrr(wrapper,"buttonComponent")
             expect(button.length).toBe(1)
+        })
+
+        it("should emit callback on click event",()=>{
+            const button = findByTestAtrr(wrapper,"buttonComponent");
+            button.simulate('click')
+            button.simulate('click')
+            const callback=mockfun.mock.calls.length
+            expect(callback).toBe(2)
         })
     })
 })
